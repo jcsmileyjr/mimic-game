e1ec16fdebdc20d3cef0110959a3f6893112811f
Smiley <jcsmileyjr@gmail.com>
2020-10-24 19:24:00 -0500

    Feature when user click on audio button the first recording plays

--- mimic-game/src/App.js
+++ mimic-game/src/App.js
@@ -9,8 +9,13 @@ import sound6 from './assets/snare.wav';
 import sound7 from './assets/tink.wav';
 import sound8 from './assets/tom.wav';
 import sound9 from './assets/hihat.wav';
+import record1 from './assets/record1.webm';
 
 function App() {
+  const playRecordingOne = () => {
+    let audio = new Audio(record1);
+        audio.play();
+  }
 
   return (
     <div className="App">
@@ -21,9 +26,9 @@ function App() {
         <h4 className="remove-top-margin">Score: 0</h4>
       </header>
       <main>        
-        <section>
+        <section className="section-divider">
           <h3>Click to Listen</h3>
-          <button>
+          <button onClick={playRecordingOne}>
             <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24">
               <path d="M0 1v22h24v-22h-24zm4 20h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2v-2h2v2zm14 12h-12v-10h12v10zm4 4h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2v-2h2v2zm-12 10v-6l5 3-5 3z"/>
             </svg>
